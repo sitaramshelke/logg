638c03bf34e8b8fed3acecd3c2a4de9ecf404bbe
sitaram-altizon <sitaram_shelke@altizon.com>
2020-08-04 20:38:41 +0530

    Add pre/post mutation feature

--- lib/process/json_processor.rb
+++ lib/process/json_processor.rb
@@ -2,10 +2,11 @@ module Process
   class JsonProcessor
     attr_reader :event_queue
 
-    def initialize(store_instance, _options)
+    def initialize(store_instance, options)
       @event_queue = Queue.new  # Thread safe queue
       @store_instance = store_instance
       @current_thread = nil
+      @options = options
     end
 
     def start
@@ -21,18 +22,48 @@ module Process
       loop do
         until @event_queue.length.zero?
           event = @event_queue.pop
-          output = process_event(event)
+          event = pre_mutations(event)
+          event = process_event(event)
+          output = post_mutations(event)
           @store_instance.write("#{output}\n")
         end
       end
     end
 
     def process_event(event)
-      str = ''
-      event.each do |k, v|
-        str += " #{k}:#{v}"
+      event
+    end
+
+    def pre_mutations(event)
+      run_mutations(event, @options['pre-mutations'] || [])
+    end
+
+    def post_mutations(event)
+      run_mutations(event, @options['post-mutations'] || [])
+    end
+
+    def run_mutations(event, mutations)
+      old = event
+      current = event
+      mutations.each do |m|
+        code = m['value'] || ''
+        y = get_lambda(code)
+        current = execute_lambda(old, y)
+        old = current
       end
-      str
+      current
+    end
+
+    def get_lambda(code)
+      eval(code)   # TODO: Safeguard this
+    rescue StandardError
+      proc { |a| a }
+    end
+
+    def execute_lambda(event, lmd)
+      lmd.call(event)
+    rescue StandardError
+      event
     end
   end
 end
